2e80c828db217c587142dfa9213892c47e469ce9
abhishekbisht908 <abhishekbisht908@gmail.com>
2025-10-12 12:31:12 +0000

    Test change

--- android/app/src/main/kotlin/com/abhishek/meditrack/medi_track/MainActivity.kt
+++ android/app/src/main/kotlin/com/abhishek/meditrack/medi_track/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.abhishek.meditrack.medi_track
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
