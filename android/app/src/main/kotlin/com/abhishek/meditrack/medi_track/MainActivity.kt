f5aa8476ff7d2323d6c6ac843b1804b37d3b1cbf
abhishekbisht908 <abhishekbisht908@gmail.com>
2025-10-12 12:44:04 +0000

    Version corrected

--- android/app/src/main/kotlin/com/abhishek/meditrack/medi_track/MainActivity.kt
+++ android/app/src/main/kotlin/com/abhishek/meditrack/medi_track/MainActivity.kt
@@ -2,9 +2,11 @@ package com.abhishek.meditrack.medi_track
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
         super.configureFlutterEngine(flutterEngine)
     }
 }
