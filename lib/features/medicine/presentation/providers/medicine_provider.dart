a6d9017cc17c606394cad1f21e903e5f1f76e2a0
abhishekbisht908 <abhishekbisht908@gmail.com>
2025-10-12 08:59:30 +0000

    corrected imports

--- lib/features/medicine/presentation/providers/medicine_provider.dart
+++ lib/features/medicine/presentation/providers/medicine_provider.dart
@@ -1,56 +1,63 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 
-import '../../../../models/medicine.dart';
-import '../../../../services/db_service.dart';
+import '../../../models/medicine.dart';
+import '../../../services/db_service.dart';
+import '../../../core/notification_service.dart';
 
-class MedicineProvider with ChangeNotifier {
-  final DBService dbService;
-  late final StreamSubscription<BoxEvent> _subscription;
+class MedicineProvider extends ChangeNotifier {
+  final DBService _dbService;
+  late final StreamSubscription _streamSubscription;
 
-  MedicineProvider(this.dbService) {
-    _subscription = dbService.watch.listen((event) {
-      // Reload medicines on any change
-      loadMedicines();
+  List<Medicine> _medicines = [];
+  List<Medicine> get medicines => _medicines;
+
+  MedicineProvider(this._dbService) {
+    _loadMedicines();
+    // Listen to changes in the database
+    _streamSubscription = _dbService.watch.listen((event) {
+      _loadMedicines();
     });
   }
 
-  List<Medicine> _medicines = [];
-
-  List<Medicine> get medicines => _medicines;
-
-  @override
-  void dispose() {
-    _subscription.cancel();
-    super.dispose();
-  }
-
-  /// Load all medicines from DB
-  Future<void> loadMedicines() async {
-    _medicines = dbService.getAllMedicines();
+  void _loadMedicines() {
+    _medicines = _dbService.getAllMedicines();
     notifyListeners();
   }
 
-  /// Add a new medicine and refresh list
   Future<void> addMedicine(Medicine medicine) async {
-    await dbService.addMedicine(medicine);
+    await _dbService.addMedicine(medicine);
+
+    // Schedule the notification
+    await NotificationService.scheduleNotification(
+      medicine.id.hashCode, // Use a unique ID for the notification
+      medicine.name,
+      'Time to take your ${medicine.dosage} dose',
+      medicine.hour,
+      medicine.minute,
+    );
+
+    notifyListeners();
   }
 
-  /// Update medicine at given index
-  Future<void> updateMedicine(int index, Medicine updated) async {
-    await dbService.updateMedicine(index, updated);
+  Future<void> updateMedicine(int index, Medicine medicine) async {
+    await _dbService.updateMedicine(index, medicine);
+    notifyListeners();
   }
 
-  /// Delete medicine by index
   Future<void> deleteMedicine(int index) async {
-    await dbService.deleteMedicine(index);
+    // Before deleting, cancel the notification
+    final medicine = _medicines[index];
+    await NotificationService.cancelNotification(medicine.id.hashCode);
+
+    await _dbService.deleteMedicine(index);
+    notifyListeners();
   }
 
-  /// Clear all medicines
-  Future<void> clearMedicines() async {
-    await dbService.clearMedicines();
+  @override
+  void dispose() {
+    _streamSubscription.cancel();
+    super.dispose();
   }
 }
